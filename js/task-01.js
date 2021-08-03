c89e433dbd581aa5955032fd57c85751130b6afd
MykytaPervushyn <80350205+MykytaPervushyn@users.noreply.github.com>
2021-08-03 14:44:13 +0300

    створює і заповнює html та js файли task-01

---
+++ js/task-01.js
@@ -0,0 +1,38 @@
+// const catEl = document.querySelector('#categories');
+// console.log(catEl);
+
+const catItemEl = document.querySelectorAll('.item');
+// console.log(catItemEl);
+
+// const lengthCatItemEl = catItemEl.length;
+// console.dir(lengthCatItemEl);
+
+
+console.log(`У списку ${catItemEl.length} категорії.`);
+
+const itemTitle = document.querySelector('.item'). firstElementChild.textContent;
+console.log('Категорія:', itemTitle);
+
+const itemsQuantiti =
+    document.querySelector('.item').lastElementChild.children.length;
+console.log('Кількість елементів:', itemsQuantiti);
+
+
+
+console.log('Категорія:', catItemEl[1].firstElementChild.textContent);
+console.log('Кількість елементів:', catItemEl[1].lastElementChild.children.length);
+
+
+
+const thirdItemTitle =
+    document.querySelector('#categories').lastElementChild.firstElementChild.textContent;
+console.log('Категорія:', thirdItemTitle);
+
+const thirdItemsQuantiti =
+    document.querySelector('#categories').lastElementChild.lastElementChild.children.length;
+console.log('Кількість елементів:', thirdItemsQuantiti);
+
+
+
+
+
